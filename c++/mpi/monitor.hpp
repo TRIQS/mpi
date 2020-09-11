b47d593f36fd6286f7ef8689daf1f5923c6d5ddb
Philipp Dumitrescu <pdumitrescu@flatironinstitute.org>
2020-09-11 19:21:48 -0400

    [monitor] polish of comments; no code change

--- c++/mpi/monitor.hpp
+++ c++/mpi/monitor.hpp
@@ -22,17 +22,17 @@
 namespace mpi {
 
   /**
-   * Contructed on top on a communicator, this class
-   * monitors the failure of node, due to e.g. exceptions.
+   * Contructed on top on a mpi communicator, this class
+   * monitors and syncronizes failure states of nodes, due to e.g. exceptions.
    *
    * Usage : 
    *   monitor M{comm};
    *
-   *   on a node : request_emergency_stop.
+   *   on a node when failure occurs : request_emergency_stop.
    *
    *   on all nodes : should_stop -> bool tells where a node a requested an emergency stop
    *
-   *   At the end, finalize() clean the monitor object and return a bool : true iif it finished normally. 
+   *   finalize() cleans the monitor object and returns a bool : true iif computation finished normally (no emergency stop). 
    */
   class monitor {
 
@@ -42,26 +42,28 @@ namespace mpi {
       MPI_Request request;
       int value = 0;
     };
-    std::vector<future> root_futures;  // the communication of local_stop from the nodes to the root. On the root only.
-    MPI_Request req_ibcast, req_isent; // request for the ibcast and isent. all nodes.
+    std::vector<future> root_futures;  // communication of local_stop from the nodes to the root. On root only.
+    MPI_Request req_ibcast, req_isent; // request for the ibcast and isent. On all nodes.
 
-    int local_stop  = 0;     // 1 if the node has requested an emergency stop. Local to the node. No bool in MPI.
-    int global_stop = 0;     // 1 if one node has requested an emergency stop. Always the same on all nodes.
-    bool finalized  = false; // the finalized should be called once,
-                             // and can not be the desctructor since we use the returned value
+    int local_stop  = 0;     // = 1 if the node has requested an emergency stop. Local to the node. (No bool in MPI.)
+    int global_stop = 0;     // = 1 if any node has requested an emergency stop. Always the same on all nodes.
+    bool finalized  = false; // the finalized() method should be called once,
+                             // and can not just be the desctructor since we use the returned value
 
     public:
-    /// Constructs on top on a communicator
+    /// Constructs on top on a mpi communicator
     monitor(mpi::communicator c) : com(c) {
       if (com.rank() == 0) { // on root
+        // Register an async recv for the variable local_stop from every non-root nodes
+        // The associated send will be issued from each node:
+        // * with value = 1 in case of emergency stop
+        // * with value = 0 during finalization
         root_futures.resize(c.size() - 1);
-        // place a async recv for local_stop from all OTHER nodes
-        // the send will be issued in case of emergency stop with value 1, or at the end with value 0
         for (int rank = 1; rank < c.size(); ++rank) { // the index of root_futures is rank - 1 since there is none for rank = 0
           MPI_Irecv(&(root_futures[rank - 1].value), 1, MPI_INT, rank, 0, MPI_COMM_WORLD, &(root_futures[rank - 1].request));
         }
       } else { // not root
-        // place the receive of the ibcast of global_stop that root will issue in case of emergency stop
+        // Register the receive of the ibcast of global_stop that root will issue (in case of emergency stop or during fianlize)
         MPI_Ibcast(&global_stop, 1, MPI_INT, 0, MPI_COMM_WORLD, &req_ibcast);
       }
     }
@@ -71,9 +73,9 @@ namespace mpi {
 
     ~monitor() { finalize(); }
 
-    /// Request an emergency stop of all nodes in the mpi Communicator.
+    /// Request an emergency stop of all nodes contained in the mpi communicator.
     /// It send the message to the root.
-    /// It the local node is the root, it sends the global bcast to all nodes to order them to stop
+    /// It the local node is the root, immedeatly sends the global ibcast to all nodes can check if to stop
     void request_emergency_stop() {
       EXPECTS(!finalized);
       if (local_stop) { return; } // prevent sending signal multiple times 
@@ -81,17 +83,18 @@ namespace mpi {
       if (com.rank() == 0) { // root
         global_stop = 1;
         MPI_Ibcast(&global_stop, 1, MPI_INT, 0, MPI_COMM_WORLD, &req_ibcast);
-      } else // non root
+      } else { // non root
         MPI_Isend(&local_stop, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &req_isent);
+      }
     }
 
-    /// Any node : should I stop my calculation ?
+    /// Any node: should the calculation should be stopped?
     [[nodiscard]] bool should_stop() {
       EXPECTS(!finalized);
       if (global_stop or local_stop) return true;
-      if (com.rank() == 0) { // root test whether other nodes have emergency stop
+      if (com.rank() == 0) { // root test whether other nodes have emergency stop and communicates signal
         root_check_nodes_and_bcast();
-      } else { // other nodes just listen to the root bcast to see if an emergency stop has been broadcasted
+      } else { // other nodes just listen to the root bcast to see if an emergency stop broadcast has occured
         MPI_Status status;
         int flag;
         MPI_Test(&req_ibcast, &flag, &status);
@@ -101,8 +104,8 @@ namespace mpi {
     }
 
     /// Finalize the monitor.
-    /// As the end of this functions, all nodes have completed their work, or had an emergency stop.
-    /// The result is guaranteed to be the same on all nodes.
+    /// At end of this functions, all nodes have completed their work, or have had an emergency stop.
+    /// The global_stop result is guaranteed to be the same on all nodes.
     bool finalize() {
       if (finalized) return not global_stop;
       EXPECTS_WITH_MESSAGE(not finalized, "Logic error : mpi::monitor::finalize can only be called once.");
@@ -111,12 +114,13 @@ namespace mpi {
         while (root_check_nodes_and_bcast()) { usleep(100); } // 100 us (micro seconds)
         // all others node have finished
         // if the root has never emitted the ibcast, we do it and wait it, since we can not cancel it FIXME (why ??).
-        if (!global_stop) MPI_Ibcast(&global_stop, 1, MPI_INT, 0, MPI_COMM_WORLD, &req_ibcast);
+        if (!global_stop) { MPI_Ibcast(&global_stop, 1, MPI_INT, 0, MPI_COMM_WORLD, &req_ibcast); }
       } else {
-        // on a node : either Isend was done when setting local_stop, or we do it now, sending local_stop = 0, i.e, work is done, and fine.
-        if (!local_stop) MPI_Isend(&local_stop, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &req_isent);
+        // on non-root node: either Isend was done when local_stop was set (during request_emergency_stop),
+        // or it has to happen now, sending local_stop = 0, i.e, work is done, and fine.
+        if (!local_stop) { MPI_Isend(&local_stop, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &req_isent); }
       }
-      // all nodes wait for the ibcast to be complete.
+      // All nodes wait for the ibcast of global_stop to be complete.
       MPI_Status status;
       MPI_Wait(&req_ibcast, &status);
       finalized = true;
