192464cc9e511c4d90fd149b05389aead8ca2ceb
Nils Wentzell <nwentzell@flatironinstitute.org>
2021-07-28 14:34:45 -0400

    Add mpi_broadcast and mpi_reduce for std::pair + test

---
+++ c++/mpi/pair.hpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2019 Simons Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0.txt
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+#include "mpi.hpp"
+#include <tuple>
+
+namespace mpi {
+
+  // ---------------- broadcast ---------------------
+
+  template <typename T1, typename T2>
+  void mpi_broadcast(std::pair<T1, T2> &p, communicator c = {}, int root = 0) {
+    broadcast(p.first, c, root);
+    broadcast(p.second, c, root);
+  }
+
+  // ---------------- reduce ---------------------
+
+  template <typename T1, typename T2>
+  auto mpi_reduce(std::pair<T1, T2> const &p, communicator c = {}, int root = 0, bool all = false, MPI_Op op = MPI_SUM) {
+    return std::make_pair(reduce(p.first, c, root, all, op), reduce(p.second, c, root, all, op));
+  }
+
+} // namespace mpi
---
+++ test/c++/mpi_pair.cpp
@@ -0,0 +1,51 @@
+// Copyright (c) 2020 Simons Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0.txt
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <mpi/pair.hpp>
+#include <mpi/string.hpp>
+#include <gtest/gtest.h>
+
+#include <complex>
+
+TEST(MPI, pair_broadcast) {
+
+  std::pair<std::string, std::complex<double>> p;
+
+  auto str  = std::string{"Hello"};
+  auto cplx = std::complex<double>(1.0, 2.0);
+
+  mpi::communicator world;
+  if (world.rank() == 0) p = {str, cplx};
+
+  mpi::broadcast(p);
+  auto [str_bc, cplx_bc] = p;
+  EXPECT_EQ(str, str_bc);
+  EXPECT_EQ(cplx, cplx_bc);
+}
+
+// -----------------------------------
+
+TEST(MPI, pair_reduce) {
+
+  mpi::communicator world;
+  auto r = world.rank();
+  auto p = std::pair{1, r};
+
+  auto [r1, r2] = mpi::all_reduce(p);
+  auto nr       = world.size();
+  EXPECT_EQ(r1, nr);
+  EXPECT_EQ(r2, nr * (nr - 1) / 2);
+}
+
+MPI_TEST_MAIN;
