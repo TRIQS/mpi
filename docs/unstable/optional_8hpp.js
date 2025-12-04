a339823553595ff6f9e8d583e45f08f5f00bb924
Wentzell <Wentzell@users.noreply.github.com>
2025-12-04 20:29:50 +0000

    Deploying to github.io from @ TRIQS/mpi@81b8ea4f5d73efc3c5e7320633c68a83c7197bc2 🚀

--- docs/unstable/optional_8hpp.js
+++ docs/unstable/optional_8hpp.js
@@ -1,6 +1,5 @@
 var optional_8hpp =
 [
     [ "mpi::mpi_broadcast", "group__coll__comm.html#ga81d6d44ba2b2a98ed9f7f04d9c2dd15a", null ],
-    [ "mpi::mpi_reduce", "group__coll__comm.html#ga717d4d20585b4cf66209f2d1a403488c", null ],
     [ "mpi::mpi_reduce_into", "group__coll__comm.html#ga8cd55671363dd9f78d1c24eed31566f3", null ]
 ];
